8c125c30ccecb139ed39bd695cb64f5fdd7f51d2
Virdzek-g <82646598+virdzek-g@users.noreply.github.com>
2022-08-17 12:50:13 -0400

    Create MAF_analysis.R

---
+++ MAF_analysis.R
@@ -0,0 +1,38 @@
+### Procssing MAF files after Mutect2 analysis
+### https://www.bioconductor.org/packages/devel/bioc/vignettes/maftools/inst/doc/maftools.html
+
+### Process Mutation Annotation Format (MAF) files for individual patients and merge them into MAF object for analysis using MAF tools
+
+setwd("")
+
+# Loop multiple files and assign name
+list_files <- dir()
+for (i in list_files) {
+  
+  # read the data
+  file <- read.maf(i)
+  file_data <- file@data
+  maf_data <- read.maf(file_data)
+  
+  # create object name based on filename
+  dataframe_name <- sub("\\-.*", "", i)
+  
+  # name the object
+  assign(dataframe_name, maf_data)
+}
+
+# create mergerd MAF object
+list <- mget(ls(pattern="PAZ")) # PAZ is a shared pattern across names of all samples
+maf <- merge_mafs(list)
+
+# Analyse merged MAF object using MAFtools
+library(maftools)
+plotmafSummary(maf = maf, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+
+oncoplot(maf = maf, top = 30, barcode_mar=8, gene_mar=7)
+
+somaticInteractions(maf = maf, top = 25, pvalue = c(0.05, 0.1))
+
+maf.titv = titv(maf = maf, plot = FALSE, useSyn = TRUE)
+#plot titv summary
+plotTiTv(res = maf.titv)
