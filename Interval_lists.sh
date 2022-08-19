14fc627e66cb579fc014bc10beaac133eeb15e67
Virdzek-g <82646598+virdzek-g@users.noreply.github.com>
2022-08-19 14:52:01 -0400

    Update Interval_lists.sh

--- Interval_lists.sh
+++ Interval_lists.sh
@@ -1,4 +1,6 @@
 # Preparation of interval list file for GATK WES analysis
+
+## BedToIntervalList
 # Picard tools require intevral_list instead of Bed files to annotate coordinates of genomic regions.
 # In this analysis are two Bed files converted to interval_lists and then combined. The aim is to combine two original interval_lists and keep only genomic regions which are present in both original interval_lists.
 
@@ -11,7 +13,6 @@ cat twist_exome.bed | sed 's/chr//g' > twist_exome_v2.bed
 # Removed contig from the bed file
 cat twist_exome_v2.bed | sed '/7_gl000195_random/d' > twist_exome_v3.bed
 
-
 # Convert a BED file to a Picard Interval List (GATK tool)
 source /software/scripts/useuse
 reuse Picard-Tools
@@ -23,9 +24,25 @@ java -jar picard.jar BedToIntervalList \
       SD=/dir/reference_hg19/Homo_sapiens_assembly19.fasta
 
 
+
+## IntervalListTools (overlaps)
 # Combine the intervals from two interval lists, and keeping only overlapping intervals (GATK tool)
 java -jar picard.jar IntervalListTools \
        ACTION=OVERLAPS \
        I=dir/twist_exome_v3.bed \
        SI=dir/IDT.interval_list \
        O=dir/combined.interval_list 
+
+
+
+## VcfToIntervalList
+# Tried to run VcfToIntervalList Picard function and received following error
+# Unable to parse header with error: Your input file has a malformed header: We never saw the required CHROM header line
+# Downloaded xyz.vcf.gz file and using samtools run
+tabix xyz.vcf.gz
+
+# then
+java -jar picard.jar VcfToIntervalList \
+      I=/dir/xyz.vcf.gz \
+      O=/dir/xyz.interval_list/
+
